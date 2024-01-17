28bf897969afb797be5447fb6c3c421f2e55dd42
Cyberrayon04846 <cyberrayon@gmail.com>
2024-01-17 13:36:09 +0530

    api error solved

--- booktrip/src/API/api.jsx
+++ booktrip/src/API/api.jsx
@@ -1,5 +1,5 @@
 const apiURL = import.meta.env.VITE_APP_API_URL;
 const apiPort = import.meta.env.VITE_APP_API_PORT;
-const API = `${apiURL}${apiPort}/server`;
+const API = `${apiURL}${apiPort}`;
 
 export default API;
