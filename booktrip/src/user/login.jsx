e16235afe07d06399a76306cfeead25718783ff7
Cyberrayon04846 <cyberrayon@gmail.com>
2024-01-17 13:36:39 +0530

    login  updated

--- booktrip/src/user/login.jsx
+++ booktrip/src/user/login.jsx
@@ -1,46 +1,27 @@
-
 import {
   React,
   viteLogo,
-  axios,
-  useNavigate,
   useState,
   useEffect,
-  toast,
   yupResolver,
   useForm,
   Link,
   useDispatch,
-  useSelector
+  useSelector,
 } from "_helper";
-import{ Loader } from '_components'
+import { Loader } from "_components";
 import * as Yup from "yup";
+import { authActions } from "../redux/auth.slice";
 import "react-toastify/dist/ReactToastify.css";
 import "../style/login.css";
 // Function to handle user login
 function Login() {
   const dispatch = useDispatch();
-  const session = useSelector((state) => state.session.value);
-  const [loading, setLoading] = useState(true);
+
   const [isChecked, setIsChecked] = useState(false);
   const [type, setType] = useState("password");
-  const toastOptions = {
-    className: "custom-toast",
-    position: "bottom-right",
-    autoClose: 3000,
-    hideProgressBar: true,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "dark",
-  };
-  useEffect(() => {
-    // Simulate loading delay
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, [])
+
+  // functions for hide and show password
   const handleToggle = () => {
     if (type === "password") {
       setType("text");
@@ -69,94 +50,98 @@ function Login() {
 
   // Handle form submission
   const onSubmit = (data) => {
-   
+    console.log("data");
     const input = {
       userName: data.userName,
       password: data.password,
     };
+    // auth action from redux store for login
     dispatch(authActions.login(input));
-    console.log(data);
   };
- 
+
   return (
     <>
-     {loading && <Loader />}
-    <div className="login">
-      <div className="login_box" >
-      <div className="login_box_header loading_animation">
-          <img src={viteLogo} alt="" className="loign_box_header_img " />
-          <h3 className="login_box_header_text ">Login to Book Trip</h3>
-        </div>
-        <div className="login_box_form loading_animation">
-        <form className="login_form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="login_input_container">
-              <label htmlFor="userName" className="form_input_lable">
-                Name
-              </label>
-              <input
-                type="text"
-                autoComplete="off"
-                name="userName"
-                className="form_input_box"
-                {...register("name")}
-              />
+      <div className="login">
+        <div className="login_box">
+          <div className="login_box_header loading_animation">
+            <img src={viteLogo} alt="" className="loign_box_header_img " />
+            <h3 className="login_box_header_text ">Login to Book Trip</h3>
+          </div>
+          <div className="login_box_form loading_animation">
+            <form className="login_form" onSubmit={handleSubmit(onSubmit)}>
+              <div className="login_input_container">
+                <label htmlFor="userName" className="form_input_lable">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  autoComplete="off"
+                  name="userName"
+                  className="form_input_box"
+                  {...register("userName")}
+                />
 
-              {errors.name && (
-                <div className="error">{errors.name.message}</div>
-              )}
-            </div>
-        <div className="login_input_container">
-              <label htmlFor="password" className="form_input_lable">
-               Password
-              </label>
-              <input
-                type={type}
-                autoComplete="off"
-                name="password"
-                className="form_input_box"
-                {...register("password")}
-              />
+                {errors.userName && (
+                  <div className="error">{errors.userName.message}</div>
+                )}
+              </div>
+              <div className="login_input_container">
+                <label htmlFor="password" className="form_input_lable">
+                  Password
+                </label>
+                <input
+                  type={type}
+                  autoComplete="off"
+                  name="password"
+                  className="form_input_box"
+                  {...register("password")}
+                />
 
-              {errors.name && (
-                <div className="error">{errors.name.message}</div>
-              )}
-              <label
-                htmlFor="myCheckbox"
-                className="checkbox-label"
-                onClick={handleCheckboxChange}
-              >
-                <span
-                  className={` ${
-                    isChecked ? "checkbox-icon-checked" : "checkbox-icon"
-                  }`}
+                {errors.password && (
+                  <div className="error">{errors.password.message}</div>
+                )}
+                <label
+                  htmlFor="myCheckbox"
+                  className="checkbox-label"
                   onClick={handleCheckboxChange}
-                ></span>
-                {` ${isChecked ? "Hide password" : "Show password"}`}
-              </label>
-            </div>
-            <div className="login_buttons loading_animation">
-              <button
-                className="login_box_button "
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Login
-              </button>
+                >
+                  <span
+                    className={` ${
+                      isChecked ? "checkbox-icon-checked" : "checkbox-icon"
+                    }`}
+                    onClick={handleCheckboxChange}
+                  ></span>
+                  {` ${isChecked ? "Hide password" : "Show password"}`}
+                </label>
+              </div>
+              <div className="login_buttons loading_animation">
+                <button
+                  className="login_box_button "
+                  type="submit"
+                  // onClick={handleSubmit}
+                >
+                  Login
+                </button>
 
-             <div className="login_bottom_box">
-             <Link to="/accounts/register" className="login_box_bottom_button">
-               Forgot Password ?
-              </Link>
-              <Link to="/accounts/register" className="login_box_bottom_button">
-               New Account
-              </Link>
-             </div>
-            </div>
-        </form>
+                <div className="login_bottom_box">
+                  <Link
+                    to="/accounts/register"
+                    className="login_box_bottom_button"
+                  >
+                    Forgot Password ?
+                  </Link>
+                  <Link
+                    to="/accounts/register"
+                    className="login_box_bottom_button"
+                  >
+                    New Account
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-       
       </div>
-    </div>
     </>
   );
 }
