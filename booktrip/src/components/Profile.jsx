93471d339dec8621461c76ecdc4294759a368a58
Cyberrayon04846 <cyberrayon@gmail.com>
2024-01-18 10:37:50 +0530

    profile details update

--- booktrip/src/components/Profile.jsx
+++ booktrip/src/components/Profile.jsx
@@ -1,37 +1,39 @@
 import React from 'react'
 import '../style/profile.css'
 function Profile() {
+  const userDetails =JSON.parse( localStorage.getItem("auth"));
+  console.log(userDetails);
   return (
    <div className="profile">
     <div className="items profile_item_1">
       <h4 className="item__title">
         Name
       </h4>
-      <p className="item__value">Preet Chahal</p>
+      <p className="item__value">{userDetails.name}</p>
     </div>
     <div className="items profile_item_2">
       <h4 className="item__title">
-        adress
+        User Name
       </h4>
-      <p className="item__value">jind mohal crsfioef wefroeiwfn</p>
+      <p className="item__value">{userDetails.username}</p>
     </div>
     <div className="items profile_item_3">
       <h4 className="item__title">
-       Pincode
+        Address
       </h4>
-      <p className="item__value">126116</p>
+      <p className="item__value">{userDetails.address}</p>
     </div>
     <div className="items profile_item_4">
       <h4 className="item__title">
        mobile no 
       </h4>
-      <p className="item__value">324325325234</p>
+      <p className="item__value">{userDetails.phoneNo}</p>
     </div>
     <div className="items profile_item_5">
       <h4 className="item__title">
         email
       </h4>
-      <p className="item__value">ewrtregw@gmaiul.co</p>
+      <p className="item__value">{userDetails.email}</p>
     </div>
     
    
