959d9f8e4e2be4627a571c8997089ef7b100c9ab
Cyberrayon04846 <cyberrayon@gmail.com>
2024-02-01 11:33:47 +0530

    Update store.js

--- booktrip/src/redux/store.js
+++ booktrip/src/redux/store.js
@@ -1,24 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {authReducer} from "./auth.slice";
-import { balanceReducer } from "./balance.slice.js"; // Correct path
+import { authReducer } from "./auth.slice";
+import { balanceReducer } from "./balance.slice.js";
 
 import pageShifter from "./pageShifter";
-import slideHandler from "./slideHandler";
 import sessionHandler from "./sessionHandler";
 
-// Export individual reducers
 export * from "./auth.slice";
 export * from "./balance.slice";
 
-
-// Create the Redux store with the specified reducers
 export const store = configureStore({
   reducer: {
-    auth : authReducer,  // Reducer for managing authentication state
-    balance : balanceReducer ,  // Reducer for managing balance state
-  
-    shifter: pageShifter,  // Page shifter utility
-    slide: slideHandler,  // Slide handler utility
-    session: sessionHandler,  // Session handler utility
+    auth: authReducer, // Reducer for managing authentication state
+    balance: balanceReducer, // Reducer for managing balance state
+    shifter: pageShifter, // Page shifter utility
+    session: sessionHandler, // Session handler utility
   },
 });
