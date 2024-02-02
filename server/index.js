0731f855dc86a90f1de7fee6280a50e32394110a
Cyberrayon04846 <cyberrayon@gmail.com>
2024-02-02 18:12:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyParser from 'body-parser';
-
+import * as fs from "fs";
 import dbConfig from './config/dbConfig.js';  // necessary to import
 
 import authRoute from "./routes/authRoutes.js";
@@ -19,19 +19,17 @@ app.use(cors());
 app.use(cookieParser());
 
 
-
-
-
-
 // Parse request bodies as JSON
 app.use(express.json());
-app.use("/auth",  authRoute);
-app.use("/balance",  balanceRoute);
+app.use("/auth", authRoute);
+app.use("/balance", balanceRoute);
+app.use("/tickets", balanceRoute);
 
 // Get the port number from the environment variable, defaulting to 5000 if not set
 const PORT = process.env.PORT || 5000;
 
+
 // Start the server
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+    console.log(`server is running on port ${PORT}`);
 });
