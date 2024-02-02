51e34e820a7e6945c73f2857bd6b0175ee03d5d4
Cyberrayon04846 <cyberrayon@gmail.com>
2024-02-02 18:12:31 +0530

    Create ticketsRoute.js

---
+++ server/routes/ticketsRoute.js
@@ -0,0 +1,16 @@
+// route setup for user authentication (signup and login)
+
+import { Router } from "express";
+import {getTickets} from "../controller/ticketscontrol.js";
+
+const router = Router();
+
+/**
+ * POST /signup
+ * Route for user signup
+ */
+
+router.get("/getticktes", getTickets);
+
+export default router;
+32855272
