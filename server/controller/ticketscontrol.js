b5a492b64ad1066f09b0f2e8e627f8972fc2bc55
Cyberrayon04846 <cyberrayon@gmail.com>
2024-02-04 13:07:01 +0530

    Update ticketscontrol.js

--- server/controller/ticketscontrol.js
+++ server/controller/ticketscontrol.js
@@ -2,6 +2,7 @@ import ticketAuth from "../models/ticketAuthSchema.js";
 export const getTickets = async (req, res) => {
         try {
             const tickets = await ticketAuth.find();
+            console.log(tickets);
             res.status(200).json(tickets);
         } catch (err) {
             res.status(500).json(err);
