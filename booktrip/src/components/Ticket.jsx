bda10426e90e065cb4c1c1469826a1f19c8f393a
Cyberrayon04846 <cyberrayon@gmail.com>
2024-02-09 12:54:51 +0530

    Update Ticket.jsx

--- booktrip/src/components/Ticket.jsx
+++ booktrip/src/components/Ticket.jsx
@@ -3,12 +3,14 @@ import '../style/ticket.css'
 function Tickets(props) {
   return (
    <div className="ticket">
-    <div className="tour_items item_1">{props.ticketNo}</div>
-    <div className="tour_items tour_item_2">Price:{props.price}</div>
-    <div className="tour_items tour_item_3">From :{props.from}</div>
-    <div className="tour_items tour_item_4">To :{props.to}</div>
-    <div className="tour_items tour_item_5">By :{props.by}</div>
-    <button className="tour_items tour_item_6">Book Now</button>
+    <div className="tour_items tour_item_1">{props.ticketNo}</div>
+    <div className="tour_items tour_item_2">Date:{props.date}</div>
+    <div className="tour_items tour_item_3">Time:{props.time}</div>
+    <div className="tour_items tour_item_4">Price:{props.price}</div>
+    <div className="tour_items tour_item_5">From :{props.from}</div>
+    <div className="tour_items tour_item_6">To :{props.to}</div>
+    <div className="tour_items tour_item_7">By :{props.by}</div>
+    <button className="tour_items tour_item_8">Book Now</button>
    </div>
   )
 }
