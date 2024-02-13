efefe00ff5eb48470ad51c6ebde517ab95cd5697
Cyberrayon04846 <cyberrayon@gmail.com>
2024-02-13 10:43:16 +0530

    Update userAuthSchema.js

--- server/models/userAuthSchema.js
+++ server/models/userAuthSchema.js
@@ -1,64 +1,43 @@
 import mongoose from "mongoose";
-
+import { Schema } from "mongoose";
 const userAuthSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        // required: true
-    },
-    userName: {
-        type: String,
-        // required: true
-    },
-    address: {
-        type: String,
-        // required: true
-    },
-    phoneNo: {
-        type: String,
-        // required: true
-    },
-    email: {
-        type: String,
-        // required: true
-    },
-    password: {
-        type: String,
-        // required: true
-    },
-    balance:{
-        type: Number,
-        default: 0
-    },
-    bookedTickets:{
-        type: [{
-            ticketNumber: {
-                type: String
-            },
-            from: {
-                type: String
-            },
-            to: {
-                type: String
-            },
-            stateRailway: {
-                type: String
-            },
-            date: {
-                type: String
-            },
-            time: {
-                type: String
-            },
-            price: {
-                type: Number
-        }}],
-        default: []
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-})
+  name: {
+    type: String,
+    // required: true
+  },
+  userName: {
+    type: String,
+    // required: true
+  },
+  address: {
+    type: String,
+    // required: true
+  },
+  phoneNo: {
+    type: String,
+    // required: true
+  },
+  email: {
+    type: String,
+    // required: true
+  },
+  password: {
+    type: String,
+    // required: true
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  bookedTickets: {
+    type: [String],
+    default: [],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const userAuth = mongoose.model('users', userAuthSchema);
+const userAuth = mongoose.model("users", userAuthSchema);
 export default userAuth;
